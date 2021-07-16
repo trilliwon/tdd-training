0bff5941f557ccb30687e2ed89f0cac2ad56f3d5
Won <trilliwon@gmail.com>
2021-07-16 22:10:33 +0900

    .

--- multiply_test.ts
+++ multiply_test.ts
@@ -1,21 +1,68 @@
-import { assertEquals } from "https://deno.land/std@0.101.0/testing/asserts.ts";
+import {
+    assert,
+    assertEquals,
+    assertNotEquals,
+} from "https://deno.land/std@0.101.0/testing/asserts.ts";
 
 console.log("TDD");
 
-class Dollar {
-    amount: number;
+class Money {
+    protected amount: number;
     constructor(amount: number) {
         this.amount = amount;
+    }
+
+    equals(object: Money): boolean {
+        const money = object as Money;
+        if (typeof this === typeof object) {
+            return this.amount === money.amount;
+        }
+        return false;
+    }
+
+    static dollar(amount: int): Dollar {
+
+    }
+}
+
+class Dollar extends Money {
+    constructor(amount: number) {
+        super(amount);
     }
 
     times(multiplier: number): Dollar {
         return new Dollar(this.amount * multiplier);
     }
 }
+
+class Franc extends Money {
+    constructor(amount: number) {
+        super(amount);
+    }
+
+    times(multiplier: number): Franc {
+        return new Franc(this.amount * multiplier);
+    }
+}
+
 Deno.test("multiplication", () => {
     const five = new Dollar(5);
-    let product = five.times(2);
-    assertEquals(10, product.amount);
-    product = five.times(3);
-    assertEquals(15, product.amount);
+    assertEquals(new Dollar(10), five.times(2));
+    assertEquals(new Dollar(15), five.times(3));
 });
+
+Deno.test("equality", () => {
+    assertEquals(new Dollar(5), new Dollar(5));
+    assertNotEquals(new Dollar(5), new Dollar(6));
+    assertEquals(new Franc(5), new Franc(5));
+    assert(!new Franc(5).equals(new Franc(6)));
+    assert(new Franc(5) !== new Dollar(5));
+});
+
+Deno.test("Franc Multiplication", () => {
+    const five = new Franc(5);
+    assertEquals(new Franc(10), five.times(2));
+    assertEquals(new Franc(15), five.times(3));
+});
+
+console.log(new Dollar(5) == new Dollar(5))
